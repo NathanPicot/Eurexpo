072c8597f98cde69e452b034bf0a88518615652c
Nathan <nathan.picot43@gmail.com>
2022-12-01 10:21:04 +0100

    commit

--- main.php
+++ main.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
 <body>
-<form action="action.php" method="post">
+<form action="action/action.php" method="post">
     <h1>Action Verite</h1>
 
     <button type="submit">Jouer</button>
