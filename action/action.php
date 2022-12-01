65f874ec57404305cd4c3fde3aa8f6c6ba0d6153
Nathan <nathan.picot43@gmail.com>
2022-12-01 14:18:10 +0100

    front-end

--- action/action.php
+++ action/action.php
@@ -1,8 +1,16 @@
 <?php
-$action = ["Faire 2 pompes.","Faire la promotion de l'IT-Akademy.","Donner 3 nom de mangas.","propose nous une action ou une vérité a ajouter"];
-$verite= ["Est-ce que tu veux devenir Developper ?","As-tu deja coder ?"];
-choice($action,$verite);
-function choice($action, $verite){
+
+$action = ["Faire 2 pompes.","Donner 3 nom de mangas.","propose nous une action ou une vérité a ajouter","Faire 10 pompes"];
+$verite= ["Est-ce que tu veux devenir Developper ?","As-tu deja coder ?","Quel age as tu ?","Quelle action ou une vérité voudrais tu ajouter?"];
+if($_POST['action']){
+    action($action);
+}elseif ($_POST['alea']){
+    alea($action,$verite);
+}elseif ($_POST['verite']){
+    verite($verite);
+}
+
+function alea($action, $verite){
     $choice_q = random_int(0,1);
     if($choice_q === 0){
         $rand = random_int(0,count($action)-1);
@@ -10,12 +18,23 @@ function choice($action, $verite){
 
         header('Location: ../main.php?action='.$question.'&type=0');
 
-
-
     }else{
         $rand = random_int(0,count($verite)-1);
         $question = $verite[$rand];
         header('Location: ../main.php?action='.$question.'&type=1');
     }
+}
+
+function action($action){
+    $rand = random_int(0,count($action)-1);
+    $question = $action[$rand];
+
+    header('Location: ../main.php?action='.$question.'&type=0');
+}
+
+function verite($verite){
+    $rand = random_int(0,count($verite)-1);
+    $question = $verite[$rand];
+    header('Location: ../main.php?action='.$question.'&type=1');
 }
 
--- main.php
+++ main.php
@@ -10,6 +10,7 @@ if($_GET['action']){
 <head>
     <meta charset="UTF-8">
     <title>Eurexpo</title>
+    <link href="css/Style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
@@ -19,21 +20,25 @@ if($_GET['action']){
 
 <body>
 <form action="action/action.php" method="post">
-    <h1 class="">Action Verite</h1>
+    <h1 class="title">Action Verite</h1>
     <section>
         <?php
         if($question){
             if($_GET['type']==0){
-                ?><label>Action : <?php echo $question;?></label> <?php
+                ?><label class="offset-4 action">Action : <?php echo $question;?></label> <?php
             }else{
-                ?><label>Verite : <?php echo $question;?></label> <?php
+                ?><label class="offset-4 action">Verite : <?php echo $question;?></label> <?php
             }
 
         }
         ?>
     </section>
+    <section class="double_button offset-3">
+        <button class="btn btn-primary btn_action  " type="submit" name="action" value="action">Action</button>
+        <button class="btn btn-primary btn_action " type="submit" name="verite" value="verite">Verite</button>
+    </section>
     <section>
-        <button class="btn btn-primary" type="submit">Jouer</button>
+        <button class="btn btn-primary offset-3 col-6 button" type="submit" name="alea" value="alea">Aléatoire</button>
     </section>
 
 </form>
